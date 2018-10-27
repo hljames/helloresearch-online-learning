3ff739cc03280d81455fa6868426bbb611d12c2f
hanglinzhou <hanglin.zhou@richmond.edu>
2018-10-27 16:25:48 -0400

    compare modified approach 2 with original approach 2

--- approach2/approach2_fix.py
+++ approach2/approach2_fix.py
@@ -41,6 +41,7 @@ def test(alg, num_arms, num_trials, num_steps, true_means):
     rewards = np.zeros(num_trials)
     arms = np.zeros((num_trials, num_steps))
     regrets = np.zeros((num_trials, num_steps))
+    cumu_regrets = np.zeros((num_trials, num_steps))
 
     for n in range(num_trials):
         alg.init_vals(num_arms)
@@ -54,34 +55,54 @@ def test(alg, num_arms, num_trials, num_steps, true_means):
                 reward = 0.0
             alg.update(chosen_arm, reward)
             rewards[n] = rewards[n] + reward
-            #regret = s * max(true_means) - rewards[n]
-            if chosen_arm == alg.best_action():
-                regret = 0.0
-            else:
-                regret = abs(max(true_means) - (rewards[n]/s))
+            regret = max(true_means) - reward
             regrets[n][s] = regret
+            if s == 0:
+              cumu_regrets[n][s] = regret
+            else:
+              cumu_regrets[n][s] = cumu_regrets[n][s-1] + regret
             total_rewards[n][s] = reward
     n_steps = np.linspace(0, num_steps, num_steps)
-    return [num_trials, n_steps, arms, rewards, regrets, total_rewards]
+    return [num_trials, n_steps, arms, rewards, regrets, total_rewards,
+        cumu_regrets]
 
 def main():
-    num_trials = 10
-    num_steps = 100
-    true_means = [0.9, 0.5]
+    num_trials = 1000
+    num_steps = 10000
+    true_means = [0.7, 0.5]
     num_arms = len(true_means) - 1
-    p = 0.2 # fixed value for now - can vary later
+    p = 1 # fixed value for now - can vary later
     alg = Solution([], p, [])
     alg.init_vals(num_arms)
     results = test(alg, num_arms, num_trials, num_steps, true_means)
-    regrets = results[4]
+    regrets = results[6]
+    avg_regret = np.zeros(num_steps)
+    for n in range(num_trials):
+      for s in range(num_steps):
+        avg_regret[s] = avg_regret[s] + regrets[n][s]
+    avg_regret = np.divide(avg_regret, num_trials)
+
+    p_1 = 0.1 # fixed value for now - can vary later
+    alg_1 = Solution([], p_1, [])
+    alg_1.init_vals(num_arms)
+    results_1 = test(alg_1, num_arms, num_trials, num_steps, true_means)
+    regrets_1 = results_1[6]
+    avg_regret_1 = np.zeros(num_steps)
+    for n in range(num_trials):
+      for s in range(num_steps):
+        avg_regret_1[s] = avg_regret_1[s] + regrets_1[n][s]
+    avg_regret_1 = np.divide(avg_regret_1, num_trials)
+    #regrets = results[6]
     # plot the results -- not sure how to do this
     fig,ax = plt.subplots()
-    regret = regrets[0]
+    #regret = regrets[0]
     x = results[1]
+    x_1 = results_1[1]
     print(x)
-    print(len(regret))
-    ax.scatter(x, regret)
+    #print(len(regret))
+    ax.scatter(x, avg_regret, color='g')
     ax.set_title("Regret over time")
+    ax.scatter(x_1, avg_regret_1, color='k')
     ax.set_xlabel("Number of steps")
     ax.set_ylabel("Regret")
     plt.show()
